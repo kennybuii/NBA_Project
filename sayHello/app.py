2fd011b3d64520a01cc3b7da554f2f6cafb67eaa
kennyb <kennyb@sfu.ca>
2022-01-25 16:42:59 -0800

    added css styling

--- sayHello/app.py
+++ sayHello/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "manbearpig_MUDMAN888"
 #test123
 @app.route("/hello")
 def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("HI " + str(request.form['name_input']) + ", great to see you!")
     return render_template("index.html")
